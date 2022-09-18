dd05505678c0fece6cf023f8b34711010c3fcb90
Sahdev Garg <36170256+djangochain@users.noreply.github.com>
2022-09-18 23:36:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.12.1"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1" # define region as per your account
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "demo-github-action"
+  acl    = "public-read"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
